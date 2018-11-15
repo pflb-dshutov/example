ebfbb63fcd154d5a4948482fec5df828ac8d547b
Dmitrii Shutov <Dmitrii_Shutov@epam.com>
2018-11-15 10:33:11 +0400

    topicsPage fix

--- pages/topicsPage.ts
+++ pages/topicsPage.ts
@@ -2,7 +2,6 @@ import { PseudoAbstractPage } from "./pseudoAbstractPage";
 import { browser, protractor, element, by} from "protractor"
 
 export class TopicsPage extends PseudoAbstractPage {
-    // public topicsHeaderElement = element(by.xpath("//section[@class='h-c-page products-navigation-list']/h1"));
     public ResearchFieldElement = element(by.xpath("//*[@id='jump-content']/div/section[2]/div/a[7]"));
 
     constructor() {
